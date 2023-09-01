14cee91712e6d71ead9036016811232b920484ce
banzhili <17698959617@163.com>
2023-09-01 17:14:31 +0800

    添加

---
+++ myJenins/src/main/java/com/example/myjenins/MyJeninsApplication.java
@@ -0,0 +1,13 @@
+package com.example.myjenins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJeninsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJeninsApplication.class, args);
+    }
+
+}
